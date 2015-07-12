8ae688887aed207be78de479dcc8fa45e2a14e68
zzt <zengzt93@gmail.com>
2015-07-12 23:03:20 +0800

    add monitor and producer and consumer

--- java/knowledge/src/thread/monitor/ConsumerProducer.java
+++ java/knowledge/src/thread/monitor/ConsumerProducer.java
@@ -1,5 +1,10 @@
 package thread.monitor;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by zzt on 7/12/15.
  * <p>
@@ -7,5 +12,36 @@ package thread.monitor;
  *
  */
 public class ConsumerProducer {
+    public static final int CAPACITY = 10;
+    public static final int THREAD_BOUND = 5;
+    ArrayList<Integer> buffer = new ArrayList<>(CAPACITY);
 
+    public void p_i() {
+        while (true) {
+
+        }
+    }
+
+    public void c_i() {
+        while (true) {
+
+        }
+    }
+
+    public void test() {
+        Random random = new Random(23);
+        ExecutorService executorService = Executors.newFixedThreadPool(random.nextInt(THREAD_BOUND));
+        int pNum = random.nextInt(THREAD_BOUND);
+        int cNum = THREAD_BOUND - pNum;
+        for (int i = 0; i < pNum; i++) {
+            executorService.submit(this::p_i);
+        }
+        for (int i = 0; i < cNum; i++) {
+            executorService.submit(this::c_i);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ConsumerProducer().test();
+    }
 }
