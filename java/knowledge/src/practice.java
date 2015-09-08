5bdc8c46a2c9561884ac95decd60f3cb1e46a5d1
zzt <zengzt93@gmail.com>
2015-09-08 22:52:15 +0800

    learn basic html

---
+++ java/knowledge/src/practice.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zzt on 9/5/15.
+ * <p>
+ * Description:
+ */
+public class practice {
+    public static void main(String[] args) {
+        System.out.format("the turth is %s\n", "turth ");
+        System.out.format("the turth is %s\n", "th is ");
+        System.out.format("the turth is %s\n", " is %s");
+    }
+}
