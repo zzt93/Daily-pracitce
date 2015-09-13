3df37ea9f8ef4201391132c9a5254bfe1a531527
zzt <zengzt93@gmail.com>
2015-09-13 23:32:57 +0800

    learn layout of website(float, clear, box, div, position -- relative, absolute, static, fixed, media query, )

--- java/knowledge/src/practice_shortcut.java
+++ java/knowledge/src/practice_shortcut.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by zzt on 9/5/15.
  * <p>
@@ -8,5 +10,7 @@ public class practice_shortcut {
         System.out.format("the turth is %s\n", "turth ");
         System.out.format("the turth is %s\n", "th is ");
         System.out.format("the turth is %s\n", " is %s");
+        final ArrayList<String> strings = new ArrayList<>();
+        strings.stream().filter(s ->  s.equals("1")).count();
     }
 }
