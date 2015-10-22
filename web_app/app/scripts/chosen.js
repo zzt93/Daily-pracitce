9a6e1d4e1a3ab06f0e5faafa4c3fd257b147986e
zzt <zengzt93@gmail.com>
2015-10-22 23:36:21 +0800

    add advice block to health fix some bug of chosen.js

--- web_app/app/scripts/chosen.js
+++ web_app/app/scripts/chosen.js
@@ -1,34 +1,62 @@
 /**
  * Created by zzt on 10/21/15.
+ *
+ * Use to emulate the behaviour of tabs
  */
 
 
-var addliChosenLisener = function () {
+function indexOfChildren(childrens, target) {
     'use strict';
 
-    var side = document.getElementById('side_nav_list');
     var i = 0;
+    for (; i < childrens.length; i++) {
+        if (childrens[i] === target) {
+            return i;
+        }
+    }
+}
+
+
+var addListChosenListener = function (listId, contentsId) {
+    'use strict';
+
+    var side = document.getElementById(listId);
+    var i = 0;
+    const className = 'tab-chosen';
+    var block = document.getElementById(contentsId);
+
 
     function setChosen(event) {
         var target = this;
 
-        function setTab() {
-            target.classList.add('tab-chosen');
-            var now = nowSelected;
-            side.children[now.getNow()].classList.remove('tab-chosen');
-            //side.children[now.changeTo()];
+
+        var index = indexOfChildren(side.children, target);
+        if (index === nowSelected.getNow()) {
+            return;
         }
         setTab();
 
-        function setContent() {
-            document.getElementById('personal').style.display = 'none';
+        setContent(index);
+
+        nowSelected.changeTo(index);
+
+        function setTab() {
+            target.classList.add(className);
+            side.children[nowSelected.getNow()].classList.remove(className);
         }
-        setContent();
+
+        function setContent(index) {
+            block.children[index].style.display = 'block';
+            block.children[nowSelected.getNow()].style.display = 'none';
+        }
+
     }
 
     for (; i < side.childElementCount; i++) {
         side.children[i].addEventListener('click', setChosen);
+        //block.children[i].style.display = 'none';
     }
+    block.children[nowSelected.getNow()].style.display = 'block';
 };
 
 var nowSelected = function () {
@@ -45,4 +73,4 @@ var nowSelected = function () {
     };
 }();
 
-addliChosenLisener();
+addListChosenListener('side_nav_list', 'tabbed-block');
