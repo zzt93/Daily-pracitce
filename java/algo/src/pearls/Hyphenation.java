693fbe6474d5801a70cef1d8b6cae9858bc8c46c
zzt <zengzt93@gmail.com>
2016-05-01 22:43:26 +0800

    finish matrix transpose add some thought about hyphenation problem

---
+++ java/algo/src/pearls/Hyphenation.java
@@ -0,0 +1,20 @@
+package pearls;
+
+/**
+ * Created by zzt on 5/1/16.
+ * <p>
+ * <h3>Problem:</h3>
+ * <li>rules for words end in 'c' is defined in testCase/word_hyphenation</li>
+ * <li>rules must be applied in order in file, i.e. the previous rule have high priority</li>
+ *
+ * <h3>Solution</h3>
+ * <ui>A reversed mapping trie of all rules: suffix map to priority</ui>
+ * <li>if the word can reach the end: finish</li>
+ * <li>if the word can't reach end:find deepest node it can reach</li>
+ * <h3>Explanation</h3>
+ * <li>deeper mean higher priority: for a previous rule must not be the suffix of latter one</li>
+ * <li>e.g. an-tic, n-tic; h-nic, n-ic; -clic, l-ic</li>
+ */
+public class Hyphenation {
+
+}
--- java/algo/src/pearls/TransposeMatrix.java
+++ java/algo/src/pearls/TransposeMatrix.java
@@ -1,5 +1,8 @@
 package pearls;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -18,45 +21,28 @@ public class TransposeMatrix {
     public static void oldTranspose(LinkedList<Integer> matrix, int n) {
         checkMatrix(matrix, n);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                Integer integer = matrix.get(i * n + j);
-                matrix.set(j * n + i, integer);
+            for (int j = i + 1; j < n; j++) {
+                final int origin = i * n + j;
+                final int sym = j * n + i;
+                Integer f = matrix.get(origin);
+                matrix.set(origin, matrix.get(sym));
+                matrix.set(sym, f);
             }
         }
         print(matrix, n);
     }
 
     private static void print(LinkedList<?> matrix, int n) {
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matrix.get(i * n + j));
+                sb.append(matrix.get(i * n + j)).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.println(sb.toString());
     }
 
-    public static void main(String[] args) {
-        correctnessTest();
-        //        timeTest();
-    }
-
-    private static void correctnessTest() {
-        test(10);
-    }
-
-    private static void timeTest() {
-        int n = 1000;
-        test(n);
-    }
-
-    private static void test(int n) {
-        LinkedList<Integer> matrix = new LinkedList<>();
-        for (int i = 0; i < n * n; i++) {
-            matrix.add(i);
-        }
-        oldTranspose(matrix, n);
-        sortTranspose(matrix, n);
-    }
 
     private static void sortTranspose(LinkedList<Integer> matrix, int n) {
         checkMatrix(matrix, n);
@@ -68,13 +54,13 @@ public class TransposeMatrix {
         Iterator<Integer> iterator = matrix.iterator();
         for (int i = 0; i < matrix.size(); i++) {
             Integer content = iterator.next();
-            res.add(new IntIJ(content, i / n, i % n));
+            res.add(new IntIJ(content, i / n, i % n, n));
         }
         return res;
     }
 
     private static void linkedListSort(LinkedList<IntIJ> matrix, int n) {
-
+        Collections.sort(matrix);
         print(matrix, n);
     }
 
@@ -86,13 +72,11 @@ public class TransposeMatrix {
 
     private static class IntIJ implements Comparable<IntIJ> {
         private final int content;
-        private final int r;
-        private final int c;
+        private final int value;
 
-        public IntIJ(int content, int r, int c) {
+        IntIJ(int content, int r, int c, int n) {
             this.content = content;
-            this.r = r;
-            this.c = c;
+            this.value = c * n + r;
         }
 
         @Override
@@ -101,13 +85,40 @@ public class TransposeMatrix {
         }
 
         @Override
-        public int compareTo(IntIJ o) {
-            if (o.content < content) {
+        public int compareTo(@NotNull IntIJ o) {
+            if (o.value < value) {
                 return 1;
-            } else if (content < o.content) {
+            } else if (value < o.value) {
                 return -1;
             }
             return 0;
         }
+    }
+
+    public static void main(String[] args) {
+        correctnessTest();
+        timeTest();
+    }
+
+    private static void correctnessTest() {
+        test(10);
+    }
+
+    private static void timeTest() {
+        int n = 100;
+        test(n);
+    }
+
+    private static void test(int n) {
+        LinkedList<Integer> matrix = new LinkedList<>();
+        for (int i = 0; i < n * n; i++) {
+            matrix.add(i);
+        }
+        long start = System.nanoTime();
+        oldTranspose(matrix, n);
+        System.out.println(System.nanoTime() - start);
+        start = System.nanoTime();
+        sortTranspose(matrix, n);
+        System.out.println(System.nanoTime() - start);
     }
 }
