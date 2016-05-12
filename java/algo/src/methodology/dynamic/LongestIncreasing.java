49ade9c71332b77451d1e86e154fbe38fb902868
zzt <zengzt93@gmail.com>
2016-05-12 22:57:35 +0800

    add learning about jvm hashCode add lis problem thought

--- java/algo/src/methodology/dynamic/LongestIncreasing.java
+++ java/algo/src/methodology/dynamic/LongestIncreasing.java
@@ -1,5 +1,7 @@
 package methodology.dynamic;
 
+import java.util.ArrayList;
+
 /**
  * Created by zzt on 5/11/16.
  * <p>
@@ -10,6 +12,21 @@ package methodology.dynamic;
  * Given a sequence S= {a1 , a2 , a3, a4, ..., an-1, an }
  * we have to find a longest subset such that for all j and i, j<i in the subset aj<ai.
  * </p>
+ * <h3>Thought:</h3>
+ * <ul>eliminate useless comparison:</ul>
+ * <li>if already found sequence a1,..., ak, a2 ~ ak need
+ * not to be the start anymore.
+ * </li>
+ * <li>Constraint: in the worst case( in reverse order), time complexity is still O(n^2)</li>
+ * <p>
+ * <ul>convert to lcs</ul>
+ * <li>sort the input to produce an increasing sequence</li>
+ * <li>compute the lcs of this two sequence</li>
  */
 public class LongestIncreasing {
+
+    public static ArrayList<Integer> lis(int[] se) {
+        ArrayList<Integer> res = new ArrayList<>();
+        return res;
+    }
 }
