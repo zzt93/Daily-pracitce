fc1dfac98c3c8c8c656a966a55ddd565f843db20
zzt <zengzt93@gmail.com>
2016-05-31 23:12:09 +0800

    add code about remainder

---
+++ java/algo/src/pearls/tuning/Remainder.java
@@ -0,0 +1,50 @@
+package pearls.tuning;
+
+import java.util.function.BiFunction;
+
+/**
+ * Created by zzt on 5/31/16.
+ * <p>
+ * <h3></h3>
+ */
+public class Remainder {
+
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.nanoTime();
+        int res = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                res += remainder(i + 1, j + 1);
+            }
+        }
+        if (res == System.nanoTime()) {
+            System.out.print(' ');
+        }
+        System.out.println(System.nanoTime() - start);
+        Thread.sleep(100);
+        start = System.nanoTime();
+        res = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                res += remainder2(i + 1, j + 1);
+            }
+        }
+        if (res == System.nanoTime()) {
+            System.out.print(' ');
+        }
+        System.out.println(System.nanoTime() - start);
+    }
+
+    private static int remainder(int i, int j) {
+        return i % j;
+    }
+
+    private static int remainder2(int i, int j) {
+        while (i > j) {
+            i -= j;
+        }
+        return i;
+    }
+
+
+}
