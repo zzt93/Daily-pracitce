25912e7784189a9c43b6caecc57685a0d4465d31
zzt <zengzt93@gmail.com>
2016-07-28 16:37:38 +0800

    fix the bug of shell sort; add test code for shell sort;

--- java/algo/src/pearls/sort/ShellSort.java
+++ java/algo/src/pearls/sort/ShellSort.java
@@ -1,5 +1,11 @@
 package pearls.sort;
 
+import com.google.common.collect.Ordering;
+import competition.utility.ArrayUtility;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by zzt on 7/19/16.
  * <p>
@@ -9,22 +15,34 @@ public class ShellSort {
 
     private static int[] gaps = {7, 3, 1};
 
-    public static void shell(int[] ints) {
+    public static void shell(Integer[] ints) {
         for (int gap : gaps) {
             gapInsertionSort(ints, gap);
         }
     }
 
-    private static void gapInsertionSort(int[] ints, int gap) {
-        int last = ints[ints.length - 1];
-        int i;
-        for (i = ints.length - 1; i >= gap; i -= gap) {
-            if (ints[i] < ints[i - gap]) {
-                ints[i] = ints[i - gap];
-            } else {
-                break;
+    private static void gapInsertionSort(Integer[] ints, int gap) {
+        for (int j = gap; j < ints.length; j++) {
+            int last = ints[j];
+            int i;
+            for (i = j; i >= gap; i -= gap) {
+                if (last < ints[i - gap]) {
+                    ints[i] = ints[i - gap];
+                } else {
+                    break;
+                }
             }
+            ints[i] = last;
         }
-        ints[i] = last;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            final Integer[] integers = ArrayUtility.randomIntegers(i, 100, -100, 200);
+            System.out.println(Arrays.toString(integers));
+            shell(integers);
+//            System.out.println(Arrays.toString(integers));
+            assert Ordering.natural().isOrdered(Arrays.asList(integers));
+        }
     }
 }
