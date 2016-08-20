02d3dee5c48736b27bfa0c84233b9f3f11fa5912
zzt <zengzt93@gmail.com>
2016-08-20 17:36:05 +0800

    add learning char trial update .gitignore

---
+++ java/knowledge/src/nio/LearnChar.java
@@ -0,0 +1,29 @@
+package nio;
+
+import java.util.Arrays;
+
+/**
+ * Created by zzt on 8/5/16.
+ * <p>
+ * <h3></h3>
+ */
+public class LearnChar {
+
+    public static void main(String[] args) {
+        char c = '测';
+        String s = "\u10437";
+        String s1 = "\uD801\uDC02";
+        int c1 = 0x10437;
+        System.out.println(Character.isLetter(c1));
+        System.out.println(Integer.toHexString(c));
+        System.out.println(s + ": " + s.length());
+        System.out.println(s1 + ": " + s1.length());
+        System.out.println(Arrays.toString(s.toCharArray()));
+        System.out.println(Arrays.toString(s1.toCharArray()));
+        final char c2 = '鱀';
+        System.out.println(c2);
+        System.out.println('c');
+    }
+
+}
+
