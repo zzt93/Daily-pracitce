e6c7248a69a224e0210c4f6fb3a1e2de1870c5aa
zzt <zengzt93@gmail.com>
2016-12-06 11:13:44 +0800

    make it runnable;

--- java/learnHadoop/src/FirstJob.java
+++ java/learnHadoop/src/FirstJob.java
@@ -5,25 +5,36 @@
  */
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class FirstJob {
     //Mapper class
     public static class LineToWordMapper extends Mapper
-            <Text,        /*Input key Type */
+            <LongWritable,        /*Input key Type */
                     Text,                /*Input value Type*/
                     Text,                /*Output key Type*/
                     Text>        /*Output value Type*/ {
 
         @Override
-        protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-            for (String word : value.toString().split("\\s")) {
-                context.write(key, new Text(value + ":" + word));
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            System.out.printf("key: " + key + " value " + value);
+            final String[] split = value.toString().split("\\s");
+            final Text key1 = new Text(split[0]);
+            for (int i = 1; i < split.length; i++) {
+                String word = split[i];
+                if (word.equals(split[0])) {
+                    context.write(key1, new Text(value));
+                    break;
+                }
             }
         }
     }
@@ -47,5 +58,15 @@ public class FirstJob {
     //Main function
     public static void main(String args[]) throws Exception {
         Configuration conf = new Configuration();
+        Job job = new Job(conf, "key word search");
+        job.setJarByClass(FirstJob.class);
+        job.setMapperClass(LineToWordMapper.class);
+        job.setCombinerClass(WordsReducer.class);
+        job.setReducerClass(WordsReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
