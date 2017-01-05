ba31c2ccde15ce105916499534026eaefdd4978e
zzt <zengzt93@gmail.com>
2017-01-05 09:12:12 +0800

    add jit inline test;

---
+++ java/knowledge/src/opt/InlineTest.java
@@ -0,0 +1,29 @@
+package opt;
+
+import java.util.Random;
+
+/**
+ * Created by zzt on 1/4/17.
+ * <p>
+ * <h3></h3>
+ */
+public class InlineTest {
+
+    private static int fib(int i) {
+        if (i == 0 || i == 1) {
+            return 1;
+        }
+        return fib(i - 1) + fib(i - 2);
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            //            fib(random.nextInt(5) + 25);
+            fib(28);
+        }
+        long endTime = System.nanoTime();
+        System.out.format("%.2f seconds elapsed.\n", (endTime - startTime) / 1000.0 / 1000 / 1000);
+    }
+}
