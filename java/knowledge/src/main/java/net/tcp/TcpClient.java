ce1780897e7b05e7bf94cd77ade3234ed60ce404
zzt93 <zengzt93@gmail.com>
2017-03-27 08:56:29 +0800

    add udp tcp socket stream test;

--- java/knowledge/src/main/java/net/tcp/TcpClient.java
+++ java/knowledge/src/main/java/net/tcp/TcpClient.java
@@ -1,7 +1,48 @@
 package net.tcp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by zzt on 17/3/24.
  */
 public class TcpClient {
+
+    private static final int MAX = 1000;
+    private Socket client = new Socket();
+    private final Scanner scanner;
+
+    public TcpClient() throws IOException {
+        client.connect(new InetSocketAddress("localhost", 9000));
+        scanner = new Scanner(System.in);
+    }
+
+    void sendRcv() throws IOException {
+        OutputStream outputStream = client.getOutputStream();
+        InputStream inputStream = client.getInputStream();
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            System.out.println("client sending: " + input);
+            outputStream.write(input.getBytes());
+            byte[] bytes = new byte[MAX];
+            int read = inputStream.read(bytes);
+            if (read >= 0) {
+                byte[] response = Arrays.copyOf(bytes, read);
+                System.out.println(new String(response));
+            }
+        }
+    }
+
+    void send() {
+        try {
+            sendRcv();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- java/knowledge/src/main/java/net/tcp/TcpServer.java
+++ java/knowledge/src/main/java/net/tcp/TcpServer.java
@@ -1,7 +1,54 @@
 package net.tcp;
 
+import com.google.common.primitives.Bytes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by zzt on 17/3/24.
  */
 public class TcpServer {
+
+    private static final int MAX = 1000;
+    private ServerSocket server = new ServerSocket(9000);
+
+    public TcpServer() throws IOException {
+    }
+
+    private void simpleReply() throws IOException {
+        Socket socket = server.accept();
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+        byte[] bytes = new byte[MAX];
+        int len;
+        while ((len = inputStream.read(bytes)) != -1) {
+            byte[] request = Arrays.copyOf(bytes, len);
+            System.out.println(Arrays.toString(request));
+            outputStream.write(Bytes.concat("server received: ".getBytes(), request));
+        }
+    }
+
+    void reply() {
+        try {
+            simpleReply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        TcpServer tcpServer = new TcpServer();
+        TcpClient tcpClient = new TcpClient();
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        service.execute(tcpClient::send);
+        service.execute(tcpServer::reply);
+        service.shutdown();
+    }
 }
