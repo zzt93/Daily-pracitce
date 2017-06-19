a20ae0f261f67c9080123336e831d00624a747cd
zzt <zengzt93@gmail.com>
2017-06-19 08:21:13 +0800

    leet code week 37

---
+++ java/algo/src/competition/leetcode/w34/NonNegIntWithoutConsecutiveOne.java
@@ -0,0 +1,40 @@
+package competition.leetcode.w34;
+
+import org.codehaus.groovy.control.messages.Message;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * Created by zzt on 5/28/17.
+ * <p>
+ * <h3></h3>
+ */
+@Deprecated
+public class NonNegIntWithoutConsecutiveOne {
+
+    public static final int MASK = 3;
+    public static final int MASK2 = 2;
+
+
+    public int findIntegers(int num) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        NonNegIntWithoutConsecutiveOne one = new NonNegIntWithoutConsecutiveOne();
+//        System.out.println((one.findIntegers(0x1)));
+//        System.out.println((one.findIntegers(0x5)));
+//        System.out.println((one.findIntegers(0x6)));
+//        System.out.println((one.findIntegers(0x7)));
+        System.out.println((one.findIntegers(0x8)));
+        System.out.println((one.findIntegers(0x9)));
+        System.out.println((one.findIntegers(0xf)));
+        System.out.println((one.findIntegers(0xabc)));
+        System.out.println((one.findIntegers(0xffffff)));
+        System.out.println((one.findIntegers(0x1ffffff)));
+    }
+
+}
---
+++ java/algo/src/competition/leetcode/w37/MaxDistanceInArray.java
@@ -0,0 +1,48 @@
+package competition.leetcode.w37;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by zzt on 6/18/17.
+ * <p>
+ * <h3></h3>
+ */
+public class MaxDistanceInArray {
+
+    private static class Tmp {
+        private int m;
+        private int index;
+
+        public Tmp(int m, int index) {
+            this.m = m;
+            this.index = index;
+        }
+
+    }
+
+    public int maxDistance(int[][] arrays) {
+        PriorityQueue<Tmp> max = new PriorityQueue<>(
+                (t1, t2) -> -Integer.compare(t1.m, t2.m)
+        );
+        PriorityQueue<Tmp> min = new PriorityQueue<>(Comparator.comparingInt(t -> t.m));
+        for (int i = 0; i < arrays.length; i++) {
+            int[] array = arrays[i];
+            min.add(new Tmp(array[0], i));
+            max.add(new Tmp(array[array.length - 1], i));
+        }
+        Tmp poll = max.poll();
+        Tmp minP = min.poll();
+        if (poll.index == minP.index) {
+            Tmp poll1 = min.poll();
+            Tmp maxP = max.poll();
+            return Math.max(poll.m - poll1.m, maxP.m - minP.m);
+        }
+        return poll.m - minP.m;
+    }
+
+    public static void main(String[] args) {
+        MaxDistanceInArray array = new MaxDistanceInArray();
+        System.out.println(array.maxDistance(new int[][]{new int[]{1, 2, 3}, new int[]{4, 5}, new int[]{1, 2, 3}}));
+    }
+}
