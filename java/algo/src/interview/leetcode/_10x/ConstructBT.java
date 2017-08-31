6f029644eac5e9dc1dcad5f95d6d40a5437056fc
zzt <zengzt93@gmail.com>
2017-08-31 21:50:52 +0800

    leetcode 104,105

---
+++ java/algo/src/interview/leetcode/_10x/ConstructBT.java
@@ -0,0 +1,35 @@
+package interview.leetcode._10x;
+
+import static competition.leetcode.w31.SubTree.TreeNode;
+
+/**
+ * Created by zzt on 8/31/17.
+ * <p>
+ * <h3></h3>
+ */
+public class ConstructBT {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, 0, preorder.length, inorder, 0, inorder.length);
+
+    }
+
+    private TreeNode buildTree(int[] preorder, int s, int e, int[] inorder, int ls, int le) {
+        if (s >= e) {
+            return null;
+        }
+        int r = preorder[s];
+        TreeNode root = new TreeNode(r);
+        int i;
+        for (i = ls; i < le; i++) {
+            int val = inorder[i];
+            if (val == r) {
+                break;
+            }
+        }
+        int leftLen = i - ls;
+        root.left = buildTree(preorder, s + 1, s + leftLen + 1, inorder, ls, i);
+        root.right = buildTree(preorder, s + leftLen + 1, e, inorder, i + 1, le);
+        return root;
+    }
+}
---
+++ java/algo/src/interview/leetcode/_10x/MaxDepBT.java
@@ -0,0 +1,20 @@
+package interview.leetcode._10x;
+
+import static competition.leetcode.w31.SubTree.TreeNode;
+
+/**
+ * Created by zzt on 8/31/17.
+ * <p>
+ * <h3></h3>
+ */
+public class MaxDepBT {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return Math.max(l, r) + 1;
+    }
+}
