1bcfc1c6cb56390259bd1a8fc5c90f861cc97ac4
zzt <zengzt93@gmail.com>
2017-11-11 08:53:58 +0800

    leetcode 154.

--- java/algo/src/interview/leetcode/_15x/MinInRotatedSortedArray2.java
+++ java/algo/src/interview/leetcode/_15x/MinInRotatedSortedArray2.java
@@ -8,22 +8,39 @@ package interview.leetcode._15x;
 public class MinInRotatedSortedArray2 {
 
     public int findMin(int[] n) {
-        int s = 0, e = n.length;
-        while (s < e - 1) {
-            if (n[s] < n[e - 1]) {
-                return n[s];
-            } else if (n[s] > n[e-1]){
-                int mid = (e + s) / 2;
-                if (n[mid] > n[s]) {
-                    s = mid + 1;
-                } else if (n[mid] < n[s]){
-                    e = mid + 1;
-                    s++;
-                }
-            } else {
-
+        int s = 0, e = n.length - 1;
+        while (s < e) {
+            int mid = (e + s) / 2;
+            if (n[mid] > n[e]) {
+                s = mid + 1;
+            } else if (n[mid] < n[e]) {
+                e = mid;
+            } else { // mid == e
+                e--;
             }
         }
-        return n[s];
+        return n[e];
+    }
+
+    public static void main(String[] args) {
+        MinInRotatedSortedArray2 m = new MinInRotatedSortedArray2();
+        System.out.println(m.findMin(new int[]{4, 4, 5, 6, 7, 1, 2, 3, 4}));
+        System.out.println(m.findMin(new int[]{3, 1, 3, 3}));
+        System.out.println(m.findMin(new int[]{3, 3}));
+        System.out.println(m.findMin(new int[]{3, 3, 1}));
+        System.out.println(m.findMin(new int[]{3, 3, 1, 3}));
+        System.out.println(m.findMin(new int[]{3, 1, 3}));
+        System.out.println(m.findMin(new int[]{3, 3, 3, 3, 3, 3, 1}));
+        System.out.println(m.findMin(new int[]{2, 1}));
+        System.out.println(m.findMin(new int[]{1}));
+        System.out.println(m.findMin(new int[]{1, 1, 1, 1, 1, 1}));
+        System.out.println(m.findMin(new int[]{1, 2}));
+        System.out.println(m.findMin(new int[]{1, 2, 3, 4, 4, 4, 5, 6, 7}));
+        System.out.println(m.findMin(new int[]{1, 1, 1, 1, 1, 2}));
+        System.out.println(m.findMin(new int[]{1, 2, 2, 2, 2, 2}));
+        System.out.println(m.findMin(new int[]{1, 2, 3}));
+        System.out.println(m.findMin(new int[]{2, 3, 1}));
+        System.out.println(m.findMin(new int[]{2, 3, 0, 1}));
+        System.out.println(m.findMin(new int[]{2, 2, 3, 0, 0, 1, 1}));
     }
 }
