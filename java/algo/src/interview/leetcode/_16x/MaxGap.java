4148fd01bc2a5c3df819018f431924d6810234d0
zzt <zengzt93@gmail.com>
2017-11-14 07:35:56 +0800

    leetcode 164.

--- java/algo/src/interview/leetcode/_16x/MaxGap.java
+++ java/algo/src/interview/leetcode/_16x/MaxGap.java
@@ -1,5 +1,7 @@
 package interview.leetcode._16x;
 
+import java.util.Arrays;
+
 /**
  * Created by zzt on 11/13/17.
  * <p>
@@ -11,6 +13,41 @@ public class MaxGap {
         if (n.length < 2) {
             return 0;
         }
+        return radix(n);
+    }
 
+    private int radix(int[] n) {
+        int bits = 10;
+        int[] tmp = new int[n.length];
+        int exp = 1;
+        int m = Arrays.stream(n).max().getAsInt();
+        for (int i = 0; m / exp > 0; i++) {
+            int[] bucket = new int[bits];
+            for (int x : n) {
+                bucket[(x / exp) % 10]++;
+            }
+            for (int y = 1; y < bucket.length; y++) {
+                bucket[y] += bucket[y - 1];
+            }
+            for (int y = n.length - 1; y >= 0; y--) {
+                int x = n[y];
+                tmp[--bucket[(x / exp) % 10]] = x;
+            }
+            System.arraycopy(tmp, 0, n, 0, n.length);
+            exp *= 10;
+        }
+        int max = 0;
+        for (int i = 1; i < n.length; i++) {
+            max = Math.max(max, n[i] - n[i - 1]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaxGap m = new MaxGap();
+        System.out.println(m.maximumGap(new int[]{1, 2, 3, 4}));
+        System.out.println(m.maximumGap(new int[]{4, 3, 2, 1}));
+        System.out.println(m.maximumGap(new int[]{6, 5, 3, 1, 8, 7}));
+        System.out.println(m.maximumGap(new int[]{2146, 2145, 2143, 2141, 2148, 2147}));
     }
 }
