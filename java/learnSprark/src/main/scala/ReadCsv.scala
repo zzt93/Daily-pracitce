a62b88cc7daa8daa32e11456f487436787f8581c
zzt <zengzt93@gmail.com>
2017-11-18 09:52:10 +0800

    add more spark sql.

--- java/learnSprark/src/main/scala/ReadCsv.scala
+++ java/learnSprark/src/main/scala/ReadCsv.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.sql.{Encoders, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Encoders, Row, SparkSession}
+
 /**
   * Created by zzt on 10/24/17.
   *
@@ -8,27 +9,36 @@ object ReadCsv {
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("Spark SQL basic example").master("local").getOrCreate()
 
-//    val payDF = spark.read.option("header", "false").csv("/home/zzt/mis/NJU/研一/cloud/IJCAI17_data/dataset/user_pay.txt")
-    val shopDF = spark.read.option("header", "false").csv("/home/zzt/mis/NJU/研一/cloud/IJCAI17_data/dataset/shop_info.txt")
-    import spark.implicits._
-    val set = Array(1,2,3).toList
-    val shopsTypes = shopDF.filter($"_c0".isin(set: _*) && ($"_c9" isNotNull)).select("_c9").distinct().map(r => r.getAs[String](0)).collect()
-    print(shopsTypes)
-    //    shopDF.write.csv("hdfs://master:9000/shop/")
+    val payDF = spark.read.option("header", "false").csv("/home/zzt/mis/NJU/研一/cloud/IJCAI17_data/dataset/user_pay.txt")
+//    val payDF = spark.read.option("header", "false").csv("hdfs://master:9000/count2/*.csv")
+//    val viewDF = spark.read.option("header", "false").csv("/home/zzt/mis/NJU/研一/cloud/IJCAI17_data/dataset/user_view.txt")
 
-//    val shopCount = payDF.select("_c1").distinct().count()
-//    println(shopCount)
-
-    // Displays the content of the DataFrame to stdout
-    //    payDF.show()
-//    payDF.printSchema()
-    //    payDF.select("_c1").show()
-//        payDF.groupBy("_c1", "_c8").count().show()
 //    payDF.groupBy("_c0", "_c1").count().write.csv("hdfs://master:9000/count2")
 //    payDF.select("_c0").distinct().write.csv("hdfs://master:9000/user/")
-//    import spark.implicits._
-//    payDF.filter($"_c0" < 150).show()
-//    payDF.filter($"age" > 21).show()
+    import spark.implicits._
+    payDF.filter($"_c2" > "2016-06-22" && $"_c2" < "2016-08-30").sort("_c2").write.csv("hdfs://master:9000/ml/pay/")
+//    viewDF.filter($"_c2" > "2016-06-22" && $"_c2" < "2016-08-30").sort("_c2").write.csv("hdfs://master:9000/ml/view/")
   }
 
+  private def showShop(payDF: DataFrame, spark: SparkSession) = {
+    import spark.implicits._
+    val shopCount = payDF.select("_c1").distinct().count()
+    println(shopCount)
+
+    payDF.show()
+    payDF.printSchema()
+    payDF.select("_c1").show()
+    payDF.groupBy("_c1", "_c8").count().show()
+    payDF.filter($"_c0" < 150).show()
+    payDF.filter($"age" > 21).show()
+  }
+
+  private def tryShop(spark: SparkSession) = {
+    val shopDF = spark.read.option("header", "false").csv("/home/zzt/mis/NJU/研一/cloud/IJCAI17_data/dataset/shop_info.txt")
+    import spark.implicits._
+    val set = Array(1, 2, 3).toList
+    val shopsTypes = shopDF.filter($"_c0".isin(set: _*) && ($"_c9" isNotNull)).select("_c9").distinct().map(r => r.getAs[String](0)).collect()
+    print(shopsTypes)
+    shopDF.write.csv("hdfs://master:9000/shop/")
+  }
 }
