b43874701d02084001df43182d9197cdd156c9e1
zzt <zengzt93@gmail.com>
2017-12-04 08:19:42 +0800

    leetcode 190 ac.

--- java/algo/src/interview/leetcode/_19x/ReverseBits.java
+++ java/algo/src/interview/leetcode/_19x/ReverseBits.java
@@ -7,17 +7,25 @@ package interview.leetcode._19x;
  */
 public class ReverseBits {
 
-    public int reverseBits(int n) {
-        long t = n;
-        t = (t >> 16) | (t << 16);
-        t = ((t & 0x00ff00ff) << 8) | ((t & 0xff00ff00) >> 8);
-        t = ((t & 0x0f0f0f0f) << 4) | ((t & 0xf0f0f0f0) >> 4);
-        t = ((t & 0x33333333) << 2) | ((t & 0xcccccccc) >> 2);
-        return (int) (((t & 0x55555555) << 1) | ((t & 0xaaaaaaaa) >> 1));
+    public int reverseBits(int t) {
+        // have to use unsigned shift
+        t = (t >>> 16) | (t << 16);
+        t = ((t & 0x00ff00ff) << 8) | ((t & 0xff00ff00) >>> 8);
+        t = ((t & 0x0f0f0f0f) << 4) | ((t & 0xf0f0f0f0) >>> 4);
+        t = ((t & 0x33333333) << 2) | ((t & 0xcccccccc) >>> 2);
+        t = ((t & 0x55555555) << 1) | ((t & 0xaaaaaaaa) >>> 1);
+        return t;
+    }
+
+    private static void test(int i) {
+        System.out.println(Integer.toBinaryString(i));
+        ReverseBits r = new ReverseBits();
+        System.out.println(Integer.toBinaryString(r.reverseBits(i)));
     }
 
     public static void main(String[] args) {
-        ReverseBits r = new ReverseBits();
-        System.out.println(r.reverseBits(2));
+        test(65536);
+        test(2);
+        test(1);
     }
 }
