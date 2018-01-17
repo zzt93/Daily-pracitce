6c17ce50a1b33eaa8cc21a74e4d2260f5d20060a
zzt <zengzt93@gmail.com>
2018-01-17 19:00:47 +0800

    leetcode w66.

--- java/algo/src/main/java/competition/leetcode/w66/SpecialBinaryString.java
+++ java/algo/src/main/java/competition/leetcode/w66/SpecialBinaryString.java
@@ -1,59 +1,71 @@
 package competition.leetcode.w66;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
 /**
- * Created by zzt on 1/7/18.
- * <p>
- * <h3></h3>
+ * Created by zzt on 1/7/18. <p> <h3></h3>
  */
 public class SpecialBinaryString {
 
-    public String makeLargestSpecial(String s) {
-        char[] cs = s.toCharArray();
-        boolean zero = false;
-        int zc = 0, oc = 0;
-        for (char c : cs) {
-            if (c == '0') {
-                zero = true;
-                zc++;
-            } else {
-                if (zero) break;
-                oc++;
-            }
-        }
-        int gap = oc - zc;
-        zc = oc = 0;
-        ArrayList<String> pairs = new ArrayList<>();
-        for (int i = gap; i < cs.length - gap; i++) {
-            char c = cs[i];
-            if (c == '0') zc++;
-            else oc++;
-            if (zc == oc) {
-                pairs.add(new String(cs, i - 2 * zc + 1, 2 * zc));
-                zc = oc = 0;
-            }
-        }
-        Comparator<String> tComparator = Comparator.naturalOrder();
-        pairs.sort(tComparator.reversed());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < gap; i++) {
-            sb.append("1");
-        }
-        for (String pair : pairs) {
-            sb.append(pair);
-        }
-        for (int i = 0; i < gap; i++) {
-            sb.append("0");
-        }
-        return sb.toString();
-    }
+  public static void main(String[] args) {
+    SpecialBinaryString s = new SpecialBinaryString();
+    System.out.println(s.makeLargestSpecial("1101101001110000"));
+    System.out.println(s.makeLargestSpecial("10110100"));
+    System.out.println(s.makeLargestSpecial("11011000"));
+    System.out.println(s.makeLargestSpecial("1011100100101100"));
+    System.out.println(s.makeLargestSpecial("1011011000"));
+  }
 
-    public static void main(String[] args) {
-        SpecialBinaryString s = new SpecialBinaryString();
-        System.out.println(s.makeLargestSpecial("10110100"));
-        System.out.println(s.makeLargestSpecial("11011000"));
-        System.out.println(s.makeLargestSpecial("1101101001110000"));
+  public String makeLargestSpecial(String s) {
+    StringBuilder sb;
+    StringBuilder last = new StringBuilder(s);
+    int l = s.length();
+    while (true) {
+      sb = new StringBuilder(last);
+
+      for (int i = 0; i < l; i++) {
+        if (sb.charAt(i) == '0') {
+          continue;
+        }
+        int m, oneC = 0, zC = 0;
+        for (m = i; m < l; m++) {
+          if (sb.charAt(m) == '0') {
+            zC++;
+          } else {
+            oneC++;
+          }
+          if (oneC == zC) {
+            break;
+          }
+        }
+        if (m + 1 >= l || sb.charAt(m + 1) == '0') {
+          continue;
+        }
+        oneC = zC = 0;
+        int e;
+        for (e = m + 1; e < l; e++) {
+          if (sb.charAt(e) == '0') {
+            zC++;
+          } else {
+            oneC++;
+          }
+          if (oneC == zC) {
+            break;
+          }
+        }
+        if (e >= l) {
+          continue;
+        }
+        String f = sb.substring(i, m + 1);
+        String st = sb.substring(m + 1, e + 1);
+        if (f.compareTo(st) < 0) {
+          sb.replace(i, e + 1, st + f);
+        }
+      }
+
+      if (sb.toString().equals(last.toString())) {
+        break;
+      }
+      last = sb;
     }
+    return sb.toString();
+  }
 }
