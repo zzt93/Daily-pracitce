c9cd408b0ce774804430d5ecaa3159743402facd
zzt <zengzt93@gmail.com>
2018-02-24 11:05:18 +0800

    leetcode 310. add string case test.

--- java/algo/src/main/java/interview/leetcode/_3xx/_31x/MinHeightTree.java
+++ java/algo/src/main/java/interview/leetcode/_3xx/_31x/MinHeightTree.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author zzt
+ *
+ * <h3>Longest Path of un-rooted tree</h3>
+ * <li>dfs/bfs twice: longest distance</li>
+ * <li>tree dp: dp[u] = max(height[u], acc--longest path with one endpoint is `u`)</li>
  */
 public class MinHeightTree {
 
@@ -19,10 +24,41 @@ public class MinHeightTree {
         new int[][]{new int[]{1, 0}, new int[]{1, 2}, new int[]{1, 3}}));
   }
 
-  // TODO 18/2/14
   public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-    return null;
+    if (n == 1) {
+      return Collections.singletonList(0);
+    }
+    Set<Integer>[] graph = new Set[n];
+    HashSet<Integer> res = new HashSet<>();
+    for (int i = 0; i < n; i++) {
+      graph[i] = new HashSet<>();
+      res.add(i);
+    }
+    for (int[] edge : edges) {
+      graph[edge[0]].add(edge[1]);
+      graph[edge[1]].add(edge[0]);
+    }
+    List<Integer> leaf = new ArrayList<>();
+    for (int i = 0; i < graph.length; i++) {
+      if (graph[i].size() == 1) {
+        leaf.add(i);
+      }
+    }
+    while (res.size() > 2) {
+      List<Integer> newLeaf = new ArrayList<>();
+      for (Integer i : leaf) {
+        Set<Integer> nei = graph[i];
+        assert nei.size() == 1;
+        int j = nei.iterator().next();
+        graph[j].remove(i);
+        if (graph[j].size() == 1) newLeaf.add(j);
+        res.remove(i);
+      }
+      leaf = newLeaf;
+    }
+    return new ArrayList<>(res);
   }
+
 
   public List<Integer> findMinHeightTreesTLE(int n, int[][] edges) {
     if (n == 1) {
@@ -57,7 +93,9 @@ public class MinHeightTree {
     for (Integer nei : graph[i]) {
       if (!visited.contains(nei)) {
         max = Math.max(max, height(nei, graph, visited, min));
-        if (max > min) return max;
+        if (max > min) {
+          return max;
+        }
       }
     }
     return max + 1;
---
+++ java/knowledge/src/main/java/basic/StringCase.java
@@ -0,0 +1,13 @@
+package basic;
+
+/**
+ * @author zzt
+ */
+public class StringCase {
+
+  public static void main(String[] args) {
+    System.out.println("I".compareToIgnoreCase("İ"));
+    System.out.println("ı".compareToIgnoreCase("i"));
+  }
+
+}
