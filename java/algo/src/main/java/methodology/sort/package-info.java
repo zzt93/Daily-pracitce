f7613771be640917fee2da142f038ef0837259cf
zzt <zengzt93@gmail.com>
2018-08-19 14:49:30 +0800

    add sort related knowledge and practice.

---
+++ java/algo/src/main/java/methodology/sort/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * - Given an array of n elements, where each element is at most k away from its target position,
+ * devise an algorithm that sorts in O(n log k) time. -- Heap Sort or Balanced BST
+ * - Iterative quick sort: core is partition, stack has range of array to partition (the range in stack become smaller)
+ * - Iterative merge sort: core is mega, stack pop two elements and put one (the range in stack become larger)
+ */
+package methodology.sort;
